0b41d9b8afbd98dd30a36974d3a73452fb7e51ae
elEma89 <61033552+elEma89@users.noreply.github.com>
2025-04-11 20:09:18 -0400

    Add files via upload

---
+++ src/components/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>404</h1>
+        <p>Ups, parece que no podemos dar con esta página, por favor, vuelve atras.</p>
+    </div>
+);
+};
+
+export default NotFound;
---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const ItemDetailContainer = () => {
+    const { productId } = useParams(); 
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+
+    const fetchProduct = new Promise((resolve) => {
+        setTimeout(() => {
+        const productDetails = { id: productId, name: "Producto Detallado", description: "Descripción de producto" };
+        resolve(productDetails);
+        }, 1000);
+    });
+
+    fetchProduct.then((data) => setProduct(data));
+    }, [productId]);
+
+    return (
+    <div>
+        {product ? (
+        <>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <button>Agregar al carrito</button>
+        </>
+        ) : (
+        <p>Cargando detalles del producto</p>
+        )}
+    </div>
+    );
+};
+
+export default ItemDetailContainer;
